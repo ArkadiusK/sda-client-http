ad8000129464e27b9a622b7f45d5be4a9fe79456
Arkadiusz.kapusciok@gmail.com <Cycolina93>
2019-09-07 13:54:49 +0200

    fix

--- src/main/java/com/sda/http/httpRequester/HttpRequesterApplication.java
+++ src/main/java/com/sda/http/httpRequester/HttpRequesterApplication.java
@@ -1,6 +1,5 @@
 package com.sda.http.httpRequester;
 
-import com.sda.domain.Beer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
