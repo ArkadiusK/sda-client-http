138449a40d31204082ec8267ede66a51ab42bc74
Arkadiusz.kapusciok@gmail.com <Cycolina93>
2019-09-07 13:39:39 +0200

    added headers for example request

--- src/main/java/com/sda/http/httpRequester/HttpRequesterApplication.java
+++ src/main/java/com/sda/http/httpRequester/HttpRequesterApplication.java
@@ -1,10 +1,13 @@
 package com.sda.http.httpRequester;
 
+import com.sda.domain.Beer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HttpRequesterApplication {
@@ -19,11 +22,14 @@ public class HttpRequesterApplication {
 
     private static void executeMySmallTest() {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://google.com",
-                String.class);
-        String body = response.getBody();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-        System.out.println(body);
+        ResponseEntity<Object> exchange = restTemplate.exchange("https://api.punkapi.com/v2/beers/1", HttpMethod.GET, entity, Object.class);
+
+        System.out.println(exchange.getBody());
     }
 
 
